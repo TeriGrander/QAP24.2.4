ccfd1bafd4438d36753688300622d0b6a1be0272
Ирина Нугис <terigrander@yandex.ru>
2025-11-21 01:14:21 +0300

    reorder tests, remove one from lesson

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -9,21 +9,18 @@ class TestCalc:
     def test_adding_success(self):
         assert self.calc.adding(self, 1, 1) == 2
 
-    def test_adding_unsuccess(self):
-        assert self.calc.adding(self, 1, 1) == 3
+    def test_subtraction_success(self):
+        assert self.calc.subtraction(self, 4, 3) == 1
 
-    def test_zero_division(self):
-        with pytest.raises(ZeroDivisionError):
-            self.calc.division(self, 1, 0)
-    
     def test_multiply_success(self):
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_division_success(self):
         assert self.calc.division(self, 6, 3) == 2
 
-    def test_subtract_success(self):
-        assert self.calc.subtraction(self, 4, 3) == 1
-    
+    def test_zero_division(self):
+        with pytest.raises(ZeroDivisionError):
+            self.calc.division(self, 1, 0)
+
     def teardown_method(self):
         print('Executing teardown method')
